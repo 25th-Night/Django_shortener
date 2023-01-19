a216496a5924aa0f6946c5c83c3cfbd27214252e
25th-Night <brown_eyed87@naver.com>
2023-01-19 20:51:41 +0900

    Fix config/urls.py to seperate url path

--- config/urls.py
+++ config/urls.py
@@ -13,8 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# if DEBUG:
-#     import debug_toolbar
+
 from django.conf.urls import include
 from shortener.views import (
     index,
@@ -23,29 +22,27 @@ from shortener.views import (
     register,
     login_view,
     logout_view,
-    url_list,
-    url_create,
-    url_change,
 )
 from django.contrib import admin
 from django.urls import path
 
 from config.settings import DEBUG
 
+if DEBUG:
+    import debug_toolbar
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index, name="index"),
     path("register", register, name="register"),
     path("login", login_view, name="login"),
-    path("urls", url_list, name="url_list"),
-    path("urls/create", url_create, name="url_create"),
-    path("urls/<str:action>/<int:url_id>", url_change, name="url_change"),
     path("logout", logout_view, name="logout"),
     path("list", list_view, name="list_view"),
     path("get_user/<int:user_id>", get_user),
+    path("urls/", include("shortener.urls.urls")),
 ]
 
-# if DEBUG:
-#     urlpatterns += [
-#         path("__debug__/", include(debug_toolbar.urls)),  # Django Debug Tool
-#     ]
+if DEBUG:
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),  # Django Debug Tool
+    ]
