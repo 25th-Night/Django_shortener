5f5e84a716a019cde5282354d41e7c91b83fdda1
25th-Night <brown_eyed87@naver.com>
2023-01-20 10:22:55 +0900

    Add url_redirect view

--- shortener/urls/views.py
+++ shortener/urls/views.py
@@ -1,9 +1,21 @@
 from django.contrib import messages
 from shortener.forms import UrlCreateForm
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from shortener.models import ShortenedUrls
 from django.contrib.auth.decorators import login_required
 from shortener.utils import url_count_changer
+
+
+def url_redirect(request, prefix, url):
+    get_url = get_object_or_404(ShortenedUrls, prefix=prefix, shortened_url=url)
+    is_permanent = False
+    target = get_url.target_url
+    if get_url.creator.organization:
+        is_permanent = True
+
+    if not target.startswith("https://") and not target.startswith("http://"):
+        target = "https://" + get_url.target_url
+    return redirect(target, permanent=is_permanent)
 
 
 def url_list(request):
