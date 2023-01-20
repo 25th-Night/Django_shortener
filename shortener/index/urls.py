8314809a4bf72685efb98370735f196907ef0edc
25th-Night <brown_eyed87@naver.com>
2023-01-20 09:15:28 +0900

    Add function 'list', 'get_user'

--- shortener/index/urls.py
+++ shortener/index/urls.py
@@ -1,4 +1,4 @@
-from shortener.index.views import index, register, login_view, logout_view
+from shortener.index.views import index, register, login_view, logout_view, get_user, list_view
 from django.contrib import admin
 from django.urls import path
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path("register", register, name="register"),
     path("login", login_view, name="login"),
     path("logout", logout_view, name="logout"),
+    path("list", list_view, name="list_view"),
+    path("get_user/<int:user_id>", get_user),
 ]
