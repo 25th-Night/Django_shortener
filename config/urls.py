ac874cfdde51d179a68d7e76c9443b50c439dd76
25th-Night <brown_eyed87@naver.com>
2023-01-20 09:13:41 +0900

    Add path of shortener/index/urls in config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -15,14 +15,6 @@ Including another URLconf
 """
 
 from django.conf.urls import include
-from shortener.views import (
-    index,
-    get_user,
-    list_view,
-    register,
-    login_view,
-    logout_view,
-)
 from django.contrib import admin
 from django.urls import path
 
@@ -33,12 +25,7 @@ if DEBUG:
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", index, name="index"),
-    path("register", register, name="register"),
-    path("login", login_view, name="login"),
-    path("logout", logout_view, name="logout"),
-    path("list", list_view, name="list_view"),
-    path("get_user/<int:user_id>", get_user),
+    path("", include("shortener.index.urls")),
     path("urls/", include("shortener.urls.urls")),
 ]
 
