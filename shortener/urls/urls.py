f5b9e578b22baf630ccae7d7acf201dcea538ee7
25th-Night <brown_eyed87@naver.com>
2023-01-25 10:45:11 +0900

    Add router and register 'urls' in router in shortener/urls/urls.py

--- shortener/urls/urls.py
+++ shortener/urls/urls.py
@@ -1,6 +1,12 @@
 from shortener.urls.views import url_change, url_list, url_create
 from django.urls import path
 
+from rest_framework import routers
+from shortener.urls.apis import *
+
+router = routers.DefaultRouter()
+router.register(r"urls", UserViewSet)
+
 urlpatterns = [
     path("", url_list, name="url_list"),
     path("create", url_create, name="url_create"),
