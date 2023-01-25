052b96242f695cef1ee43c6ba023767d5f1a4293
25th-Night <brown_eyed87@naver.com>
2023-01-25 17:22:30 +0900

    Add update_telegram_username api

--- shortener/users/apis.py
+++ shortener/users/apis.py
@@ -1,5 +1,5 @@
 from typing import List
-from shortener.schemas import Users as U
+from shortener.schemas import Users as U, TelemgramUpdateSchema
 from shortener.models import Users
 from ninja.router import Router
 
@@ -11,3 +11,12 @@ user = Router()
 def get_user(request):
     a = Users.objects.all()
     return list(a)
+
+
+@user.post("", response={201: None})
+def update_telegram_username(request, body: TelemgramUpdateSchema):
+    user = Users.objects.filter(user_id=request.user.id)
+    if not user.exists():
+        return 404, {"msg": "No user found"}
+    user.update(telegram_username=body.username)
+    return 201, None
