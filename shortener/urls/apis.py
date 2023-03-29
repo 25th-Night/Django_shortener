ce69538826295affde689c376d0088be9ba13083
25th-Night <brown_eyed87@naver.com>
2023-03-29 22:06:24 +0900

    Add two api-funcs of statistics

--- shortener/urls/apis.py
+++ shortener/urls/apis.py
@@ -1,5 +1,5 @@
-from shortener.models import ShortenedUrls, Users
-from shortener.urls.serializers import UserSerializer, UrlListSerializer, UrlCreateSerializer
+from shortener.models import ShortenedUrls, Users, Statistic
+from shortener.urls.serializers import UserSerializer, UrlListSerializer, UrlCreateSerializer, BrowerStatSerializer
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
@@ -9,8 +9,10 @@ from rest_framework.renderers import JSONRenderer
 from django.http.response import Http404
 from rest_framework import status
 from rest_framework.response import Response
-from shortener.utils import MsgOk, url_count_changer
+from shortener.utils import MsgOk, url_count_changer, get_kst
 from rest_framework.decorators import action, renderer_classes
+from datetime import timedelta
+from django.db.models.aggregates import Count, Min
 
 
 class UrlListView(viewsets.ModelViewSet):
@@ -57,14 +59,22 @@ class UrlListView(viewsets.ModelViewSet):
         return Response(serializer.data)
 
     @action(detail=True, methods=["get", "post"])
-    def add_click(self, request, pk=None):
-        queryset = self.get_queryset().filter(pk=pk, creator_id=request.user.id)
-        if not queryset.exists():
-            raise Http404
-        rtn = queryset.first().clicked()
-        serializer = UrlListSerializer(rtn)
-        return Response(serializer.data)
+    def add_browser_today(self, request, pk=None):
+        queryset = self.get_queryset().filter(pk=pk, creator_id=request.user.id).first()
+        new_history = Statistic()
+        new_history.record(request, queryset, {})
+        return MsgOk()
 
     @action(detail=True, methods=["get"])
-    def remove_click(self, request, pk=None):
-        print("removed")
+    def get_browser_stats(self, request, pk=None):
+        queryset = Statistic.objects.filter(
+            shortened_url_id=pk,
+            shortened_url__creator_id=request.user.id,
+            created_at__gte=get_kst() - timedelta(days=14),
+        )
+        if not queryset.exists():
+            raise Http404
+        browers = queryset.values("web_browser", "created_at__date").annotate(count=Count("id")).values("count", "web_browser", "created_at__date").order_by("-created_at__date")
+        browers = queryset.values("web_browser").annotate(count=Count("id")).values("count", "web_browser").order_by("-count")
+        serializer = BrowerStatSerializer(browers, many=True)
+        return Response(serializer.data)
