b2a11e97ccfe133a381e40e07f9f3bc0f2205ea0
25th-Night <brown_eyed87@naver.com>
2023-05-03 10:19:48 +0900

    Add cache.delete on url creation

--- shortener/urls/apis.py
+++ shortener/urls/apis.py
@@ -24,6 +24,7 @@ class UrlListView(viewsets.ModelViewSet):
         # POST METHOD
         serializer = UrlCreateSerializer(data=request.data)
         if serializer.is_valid():
+            cache.delete(f"url_lists_{request.users_id}")
             rtn = serializer.create(request, serializer.data)
             return Response(UrlListSerializer(rtn).data, status=status.HTTP_201_CREATED)
         pass
