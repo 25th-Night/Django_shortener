9036bf29ec36454b6eb92005811895ebfa48f4eb
25th-Night <brown_eyed87@naver.com>
2023-05-03 10:22:10 +0900

    Add visitor func to url_list func

--- shortener/urls/views.py
+++ shortener/urls/views.py
@@ -13,6 +13,7 @@ from django.views.decorators.cache import never_cache
 from django.views.decorators.cache import cache_page
 from shortener.urls.telegram_handler import command_handler
 from shortener.urls.decorators import admin_only
+from shortener.ga import visitors
 
 
 @ratelimit(key="ip", rate="3/m")
@@ -39,7 +40,8 @@ def url_redirect(request, prefix, url):
 @login_required
 @admin_only
 def url_list(request):
-    command_handler()
+    # command_handler()
+    visitors()
     return render(request, "url_list.html", {})
 
 
